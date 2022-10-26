1c27bfedb3d72be0216b71dbddabdd40305a0403
A00466984 <shweta.dalal@smu.ca>
2022-10-25 21:10:33 -0300

    Prog assign 1

---
+++ ProgAssign1/Assignment1/Assignment1/DirWalker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment1
+{
+  
+
+    public class DirWalker
+    {
+        public int SkippedCount { get; set; }
+        public int TotalCount { get; set; }
+        public void walk(String path)
+        {
+
+            string[] list = Directory.GetDirectories(path);
+
+
+            if (list == null) return;
+
+            foreach (string dirpath in list)
+            {
+                if (Directory.Exists(dirpath))
+                {
+                    walk(dirpath);
+                    //Console.WriteLine("Dir:" + dirpath);
+                }
+            }
+            string[] fileList = Directory.GetFiles(path);
+            foreach (string filepath in fileList)
+            {
+
+                //Console.WriteLine("File:" + filepath);
+               // String path1 = "E:/MCDA/DotNet/MCDA5510_Assignments/Sample Data/2017/11/8";
+
+                SimpleCSVParser parser = new SimpleCSVParser();
+                
+                parser.parse(filepath, out int skipCount, out int total);
+
+                SkippedCount += skipCount;
+                TotalCount += total;
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            DirWalker fw = new DirWalker();
+            var folderpath = "E:\\MCDA\\DotNet\\MCDA5510_Assignments\\Sample Data";
+            
+            //StreamWriter sw = new StreamWriter();
+            // OpenStream(folderpath);
+            fw.walk(folderpath);
+            watch.Stop();
+            var logpath = "E:/MCDA/DotNet/MCDA5510_Assignments/Assignment1/Assignment1/Logs.txt";
+            using (StreamWriter writer = new StreamWriter(logpath, true))
+            {
+
+                writer.WriteLine("total skipped rows: " + fw.SkippedCount);
+                writer.WriteLine("total rows: " + fw.TotalCount);
+                writer.WriteLine("Total Execution Time: " + watch.ElapsedMilliseconds + " ms");
+            }
+        }
+
+    }
+}
---
+++ ProgAssign1/Assignment1/Assignment1/SimpleCSVParser.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using System.Linq;
+
+namespace Assignment1
+{
+    public class SimpleCSVParser
+    {
+
+
+        //public static void Main(String[] args)
+        //{
+
+        //    SimpleCSVParser parser = new SimpleCSVParser();
+        //    parser.parse(@"E:/MCDA/DotNet/MCDA5510_Assignments/Sample Data/2017/11/8/CustomerData0.csv");
+        //}
+
+        public void parse(String fileName,out int skippedCount, out int validCount)
+        {
+            skippedCount = 0;
+            validCount = 0;
+            try {
+                var watch = new System.Diagnostics.Stopwatch();
+                //watch.Start();
+                using (TextFieldParser parser = new TextFieldParser(fileName))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                   
+                    while (!parser.EndOfData)
+                {
+                    //Process row
+                    string[] fields = parser.ReadFields();
+                        //var writer = new StreamWriter(File.OpenWrite("D:\\work\\POCs\\POC_RESOURCES\\Pricing_Files\\index-edited-ap-east-1.csv"));
+                        //foreach (string field in fields)
+                        //{
+                    string value = "";
+                    
+                    int pos = Array.IndexOf(fields, value);
+                        if (pos > -1)
+                        {
+                            skippedCount++;
+                        }
+                        else
+                        {
+                           var filepath = "E:/MCDA/DotNet/MCDA5510_Assignments/Assignment1/Assignment1/Solution.csv";
+                            
+                            var line = string.Join(",", fields);
+                            bool hasText = File.ReadLines(filepath).Any(l => l.Contains(line, StringComparison.Ordinal));  //to avoid repeating headers
+                            if (hasText)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                //Console.WriteLine(line);
+                                using (StreamWriter writer = new StreamWriter(filepath, true))
+                                {
+
+                                    writer.WriteLine(line);
+                                }
+                                validCount++;
+                                //writer.WriteLine(field);
+
+                                // }
+                            }
+                        }
+                }
+                    //watch.Stop();
+                    //var logpath = "E:/MCDA/DotNet/MCDA5510_Assignments/Assignment1/Assignment1/Logs.txt";
+                    //using (StreamWriter writer = new StreamWriter(logpath, true))
+                    //{
+                        
+                    //    writer.WriteLine("Total Skipped rows: "+skipped_rows);
+                    //    writer.WriteLine("Total Valid Rows: "+valid_rows);
+                    //    writer.WriteLine("Total Execution Time: " + watch.ElapsedMilliseconds + " ms");
+                    //}
+                }
+        
+        }catch(IOException ioe){
+                Console.WriteLine(ioe.StackTrace);
+         }
+
+    }
+
+
+    }
+}
